861796cdc0a1d80d2d02e3e9971ffe15c6705ade
unknown <akshaypanchal67@gmail.com>
2020-11-09 23:25:29 -0500

    added styling to buttons and input field

--- src/components/input/input.component.js
+++ src/components/input/input.component.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 
 
@@ -37,21 +39,21 @@ const Input = ({searchFlightDetails}) => {
         // Clear the local store of originCity and destinationCity to empty
         setOriginCity("");
         setDestinationCity("");
-        setDepartureDate("");
+        setDepartureDate(""); 
 
     }
-
+ 
     return (
-        <div>
+        <Form>
             {/* creating the input and button for  user interaction */}
-            <label>Origin: </label>
-            <input id="originCity" onChange={addOriginCity} placeholder="Enter the City" value={originCity} required />
-            <label>Destination: </label>
-            <input id="destinationCity" onChange={addDestinationCity} placeholder="Enter the City" value={destinationCity} required/>
-            <label>Departure Date:</label>
-            <input id="departureDate" placeholder="yyyy/mm/dd"  onChange={addDepartureDate} value={departureDate}/>
-            <button onClick={addFlightDetails}>Search the Flights</button>
-        </div>
+            <Form.Label>Origin: </Form.Label>
+            <Form.Control id="originCity" onChange={addOriginCity} placeholder="Enter the City" value={originCity} required />
+            <Form.Label>Destination: </Form.Label>
+            <Form.Control id="destinationCity" onChange={addDestinationCity} placeholder="Enter the City" value={destinationCity} required/>
+            <Form.Label>Departure Date:</Form.Label>
+            <Form.Control id="departureDate" placeholder="yyyy/mm/dd"  onChange={addDepartureDate} value={departureDate}/>
+            <Button variant="success" onClick={addFlightDetails}>Search the Flights</Button>
+        </Form>
     );
 }
 
